c4cc71661e80ac0d83c44e581441b9cadd256396
yassinedevop <62672904+yassinedevop@users.noreply.github.com>
2020-09-30 17:20:18 +0000

    Update shape.h

--- Slider/shape_lib/shape.h
+++ Slider/shape_lib/shape.h
@@ -14,7 +14,7 @@
 void circle(float x , float y , float radius, int fill);
 
 void line(float x1, float y1 , float x2, float y2);
-
-
+void circle_stroke(float x , float y , float radius, float stroke);
+void line_stroke(float x1, float y1 , float x2, float y2,float stroke);
 
 #endif /* SHAPE_LIB_SHAPE_H_ */
